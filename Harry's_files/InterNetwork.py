49945cb078d39e356249b9d49ecde254de4125de
Harry-Westwood <56080854+Harry-Westwood@users.noreply.github.com>
2020-02-15 22:23:17 +0000

    Decay and momentum added and reg patch
    Can now use decay and momentum with SGD.
    Also patched issue with how "None" regularizations in a list are passed to neuralstellar

--- Harry's_files/InterNetwork.py
+++ Harry's_files/InterNetwork.py
@@ -1,3 +1,7 @@
+#add that it prints which leg is currently being done and use the history to print that leg's properties
+#add the extended isochrone to the large end plot
+#...unlikely you'll need to change it so that you can freely changed between MSE and MAE whilst training. 
+
 from datetime import datetime
 import numpy as np
 
@@ -11,6 +15,8 @@ from tensorflow.keras.constraints import max_norm
 from tensorflow.keras import layers
 from tensorflow.keras.regularizers import l1, l2
 import pickle
+from shutil import copyfile
+
 
 import smtplib, ssl
 from email.mime.multipart import MIMEMultipart
@@ -24,8 +30,8 @@ def set_seed(self, seed):
     np.random.seed(seed)
     tf.random.set_random_seed(seed)
 
-def Leg_Setup(folder_name,reg,lr,epoch_no,batch_size,opt,activation,dropout):
-    leg_num, list_params = Get_Num_Legs(reg=reg,lr=lr,epoch_no=epoch_no,batch_size=batch_size,opt=opt,activation=activation,dropout=dropout)
+def Leg_Setup(folder_name,reg,lr,epoch_no,batch_size,opt,activation,dropout,momentum,decay,mean_error_type,recompile=False):
+    leg_num = Get_Num_Legs(reg=reg,lr=lr,epoch_no=epoch_no,batch_size=batch_size,opt=opt,activation=activation,dropout=dropout, momentum=momentum, decay=decay, mean_error_type=mean_error_type)
     legs = []
     for leg in range(leg_num):
         leg_dict = {'contNN': False,'changeingNNparam':False}
@@ -33,11 +39,20 @@ def Leg_Setup(folder_name,reg,lr,epoch_no,batch_size,opt,activation,dropout):
         if leg > 0:
             leg_dict['contNN'] = True
 
+        #if always_recompile == True:
+        #    leg_dict['changeingNNparam'] = True
+        if type(recompile) is list:
+            try:
+                 leg_dict['changeingNNparam'] = recompile[leg]    
+            except IndexError:
+                #leg_dict['changeingNNparam'] = recompile[-1]
+                pass
+        else:
+            leg_dict['changeingNNparam'] = recompile
+
+        
         if reg != None:
             if type(reg) is list:
-                if type(reg[0]) is not list:
-                    leg_dict['reg'] = reg
-                if type(reg[0]) is list:
                     try:
                          leg_dict['reg'] = reg[leg]
                          if leg > 0:
@@ -48,19 +63,6 @@ def Leg_Setup(folder_name,reg,lr,epoch_no,batch_size,opt,activation,dropout):
         else:
                 leg_dict['reg'] = None
                 
-        if type(lr) is not list:
-            leg_dict['lr'] = lr
-        if type(epoch_no) is not list:
-            leg_dict['epoch_no'] = epoch_no
-        if type(batch_size) is not list:
-            leg_dict['batch_size'] = batch_size
-        if type(opt) is not list:
-            leg_dict['opt'] = opt
-        if type(activation) is not list:
-            leg_dict['activation'] = activation
-        if type(dropout) is not list:
-            leg_dict['dropout'] = dropout
-
         if type(lr) is list:
             try:
                  leg_dict['lr'] = lr[leg]
@@ -69,18 +71,24 @@ def Leg_Setup(folder_name,reg,lr,epoch_no,batch_size,opt,activation,dropout):
                          leg_dict['changeingNNparam'] = True
             except IndexError:
                 leg_dict['lr'] = lr[-1]
+        else:
+            leg_dict['lr'] = lr
 
         if type(epoch_no) is list:
             try:
                  leg_dict['epoch_no'] = epoch_no[leg]
             except IndexError:
                 leg_dict['epoch_no'] = epoch_no[-1]
+        else:
+            leg_dict['epoch_no'] = epoch_no
 
         if type(batch_size) is list:
             try:
                  leg_dict['batch_size'] = batch_size[leg]
             except IndexError:
-                leg_dict['batch_size'] = batch_size[-1]
+                leg_dict['batch_size'] = batch_size[-1]   
+        else:
+            leg_dict['batch_size'] = batch_size
 
         if type(opt) is list:
             try:
@@ -89,7 +97,9 @@ def Leg_Setup(folder_name,reg,lr,epoch_no,batch_size,opt,activation,dropout):
                      if opt[leg] != opt[leg-1]:
                          leg_dict['changeingNNparam'] = True
             except IndexError:
-                leg_dict['opt'] = opt[-1]
+                leg_dict['opt'] = opt[-1]   
+        else:
+            leg_dict['opt'] = opt
 
         if type(activation) is list:
             try:
@@ -98,7 +108,9 @@ def Leg_Setup(folder_name,reg,lr,epoch_no,batch_size,opt,activation,dropout):
                      if activation[leg] != activation[leg-1]:
                          leg_dict['changeingNNparam'] = True
             except IndexError:
-                leg_dict['activation'] = activation[-1]
+                leg_dict['activation'] = activation[-1]   
+        else:
+            leg_dict['activation'] = activation
 
         if type(dropout) is list:
             try:
@@ -107,12 +119,60 @@ def Leg_Setup(folder_name,reg,lr,epoch_no,batch_size,opt,activation,dropout):
                      if dropout[leg] != dropout[leg-1]:
                          leg_dict['changeingNNparam'] = True
             except IndexError:
-                leg_dict['dropout'] = dropout[-1]
+                leg_dict['dropout'] = dropout[-1]    
+        else:
+            leg_dict['dropout'] = dropout
+
+        if type(momentum) is list:
+            try:
+                 leg_dict['momentum'] = momentum[leg]
+                 if leg > 0:
+                     if momentum[leg] != momentum[leg-1]:
+                         leg_dict['changeingNNparam'] = True
+            except IndexError:
+                leg_dict['momentum'] = momentum[-1]    
+        else:
+            leg_dict['momentum'] = momentum
+
+        if type(decay) is list:
+            try:
+                 leg_dict['decay'] = decay[leg]
+                 if leg > 0:
+                     if decay[leg] != decay[leg-1]:
+                         leg_dict['changeingNNparam'] = True
+            except IndexError:
+                leg_dict['decay'] = decay[-1]    
+        else:
+            leg_dict['decay'] = decay
+
+        if type(mean_error_type) is list:
+            try:
+                 leg_dict['mean_error_type'] = mean_error_type[leg]
+                 if leg > 0:
+                     if mean_error_type[leg] != mean_error_type[leg-1]:
+                         leg_dict['changeingNNparam'] = True
+            except IndexError:
+                leg_dict['mean_error_type'] = mean_error_type[-1]   
+        else:
+                leg_dict['mean_error_type'] = mean_error_type
 
         legs.append(leg_dict)
     return legs
         
-def NN_run(folder_name, df, contNN, changeingNNparam,hidden_layers,activation,reg,dropout,opt,lr,epoch_no,batch_size,leg):
+def NN_run(folder_name, df, hidden_layers, legs, leg):
+    contNN = legs[leg]['contNN']
+    changeingNNparam = legs[leg]['changeingNNparam']
+    activation = legs[leg]['activation']
+    reg = legs[leg]['reg']
+    dropout = legs[leg]['dropout']
+    opt = legs[leg]['opt']
+    lr = legs[leg]['lr']
+    epoch_no = legs[leg]['epoch_no']
+    batch_size = legs[leg]['batch_size']
+    decay = legs[leg]['decay']
+    momentum = legs[leg]['momentum']
+    mean_error_type = legs[leg]['mean_error_type']
+
     if contNN == True and changeingNNparam == True:
         m0=NNmodel('evo',['mass', 'age', 'feh', 'Y', 'MLT'], ['L', 'Teff', 'delnu']) #not compiling NN, but different...who knows???
         m0.loadModel('drive/My Drive/4th Year Project/{}/{}_best_model.h5'.format(folder_name,folder_name),summary = False)
@@ -124,13 +184,17 @@ def NN_run(folder_name, df, contNN, changeingNNparam,hidden_layers,activation,re
     arch = [len(x_cols)]+hidden_layers+[len(y_cols)]
     m1=NNmodel('evo',['mass', 'age', 'feh', 'Y', 'MLT'], ['L', 'Teff', 'delnu'])
 
-    if (contNN == True and changeingNNparam == True) or (contNN == False and changeingNNparam == False):
+    if contNN == True:
+        m1.loadHist('drive/My Drive/4th Year Project/{}/Hist{}'.format(folder_name,folder_name),'pickle')
+        start_epoch = m1.history['legs'][-1]['epoch_no']
+
+    if (contNN == True and changeingNNparam == True) or contNN == False:
         #m1.buildModel(arch=arch, activation='swish',reg=None,dropout=None)
         if leg == 0:
             m1.buildModel(arch=arch, activation=activation,reg=reg,dropout=dropout, summary = True)
         else:
             m1.buildModel(arch=arch, activation=activation,reg=reg,dropout=dropout, summary = False)
-        m1.compileModel(opt=opt, lr=lr, loss='MAE', metrics=['MAE','MSE'], beta_1=0.9995, beta_2=0.999) #
+        m1.compileModel(opt=opt, lr=lr, loss=mean_error_type, metrics=['MAE','MSE'], beta_1=0.9995, beta_2=0.999,decay=decay,momentum=momentum) #
         #m1.compileModel(opt='SGD', lr=0.0000001, loss='MAE', metrics=['MAE','MSE'], beta_1=0.9995, beta_2=0.999)
 
     if contNN == True and changeingNNparam == False:
@@ -138,13 +202,11 @@ def NN_run(folder_name, df, contNN, changeingNNparam,hidden_layers,activation,re
 
     if contNN == True and changeingNNparam == True: 
         m1.setWeights(m0.model)
-    if contNN == True:
-        m1.loadHist('drive/My Drive/4th Year Project/{}/Hist{}'.format(folder_name,folder_name),'pickle')
-        start_epoch = m1.history['legs'][-1]['epoch_no']
 
     #m1.fitModel(df=df, cols=[x_cols,y_cols], epoch_no=1000, batch_size=int(len(df.index)), save_name='drive/My Drive/4th Year Project/{}/{}'.format(folder_name,new_file_version), callback=['mc'])
     m1.fitModel(df=df, cols=[x_cols,y_cols], epoch_no=epoch_no, batch_size=batch_size, save_name='drive/My Drive/4th Year Project/{}/{}'.format(folder_name,folder_name), callback=['mc'])
     m1.saveHist('drive/My Drive/4th Year Project/{}/Hist{}'.format(folder_name,folder_name))
+    copyfile('drive/My Drive/4th Year Project/{}/{}_best_model.h5'.format(folder_name,folder_name), 'drive/My Drive/4th Year Project/{}/{}_prev_leg_best_model.h5'.format(folder_name,folder_name)) #saves best model from last completed leg in case of crashes
     return start_epoch, m1
 
 def NN_results(grid_file, folder_name,starttime,start_epoch,last_leg=False):
@@ -152,28 +214,31 @@ def NN_results(grid_file, folder_name,starttime,start_epoch,last_leg=False):
     m1=NNmodel('evo',['mass', 'age', 'feh', 'Y', 'MLT'], ['radius','Teff', 'delnu'], Teff_scaling=5000)
     m1.loadModel('drive/My Drive/4th Year Project/{}/{}_best_model.h5'.format(folder_name,folder_name), summary = False)
     m1.loadHist('drive/My Drive/4th Year Project/{}/Hist{}'.format(folder_name,folder_name),'pickle')
-    final_epoch = m1.history['legs'][-1]['epoch_no']
+    final_epoch = m1.history['legs'][-1]['epoch_range'][1]
     #
     m1.plotHist(plot_MSE=False, this_train=False, savefile='drive/My Drive/4th Year Project/{}'.format(folder_name), trial_no=folder_name)
-    m1.plotHist(plot_MSE=False, epochs=[start_epoch,final_epoch],this_train=False) 
+    m1.plotHist(plot_MSE=False, epochs=m1.history['legs'][-1]['epoch_range'],this_train=False) 
     
     last_loss = m1.lastLoss('mean_absolute_error')
+
+    grid1=stellarGrid(grid_file)
+    grid1.loadData()
+    grid1.popIndex(['Unnamed: 0','star_mass','star_age','star_feh','initial_Yinit','initial_MLT','luminosity','effective_T','delta_nu'])
+    grid1.initialData()
     if last_leg == False:
+        m1.plotIsochrone(grid1, np.linspace(1,11,12), ['age','feh','Y','MLT'], [0, 0.28,1.9], widths=[['r',0.05],['a',0.1],['a',0.01],['a',0.1]],savefile='drive/My Drive/4th Year Project/{}'.format(folder_name), trial_no=folder_name,extended=False)
         print(last_loss)
+        
     #m1.plotAll(grid1, savefile=folder_no, trial_no=trial_no)
 
     if last_leg == True:
-        grid1=stellarGrid(grid_file)
-        grid1.loadData()
-        grid1.popIndex(['Unnamed: 0','star_mass','star_age','star_feh','initial_Yinit','initial_MLT','luminosity','effective_T','delta_nu'])
-        grid1.initialData()
         dexvals = m1.getDex(grid1)
         evaluation = m1.evalData(grid1, grid1.data['track_no'].unique().astype('int'))
         evaluation_loss = evaluation[1]
         runresults = [str(last_loss),str(evaluation_loss),str(dexvals['radius']),str(dexvals['Teff']),str(dexvals['delnu'])]
 
         sheets_results = ' '.join(runresults)
-        m1.plot_Loss_Error_HR_Iso_in_one(grid=grid1, iso_ages=np.linspace(1,11,12), extended=False, this_train = False, indices=['age','feh','Y','MLT'], isos=[0, 0.28,1.9], plot_MSE=False, epochs=None, widths=[['r',0.05],['a',0.1],['a',0.01],['a',0.1]], N=5000, one_per_track=False, track_no=100, savefile='drive/My Drive/4th Year Project/{}'.format(folder_name), trial_no=folder_name)
+        m1.plot_Loss_Error_HR_Iso_in_one(grid=grid1, iso_ages=np.linspace(1,11,12), indices=['age','feh','Y','MLT'], isos=[0, 0.28,1.9], plot_MSE=False, epochs=None, widths=[['r',0.05],['a',0.1],['a',0.01],['a',0.1]], N=5000, one_per_track=False, track_no=100, savefile='drive/My Drive/4th Year Project/{}'.format(folder_name), trial_no=folder_name)
         print(sheets_results)
         strFrom = 'astroneuralnetworks@gmail.com'
         strTo = 'accelerationduetogravity9.81@gmail.com'
@@ -211,9 +276,8 @@ def NN_results(grid_file, folder_name,starttime,start_epoch,last_leg=False):
             server.sendmail(strFrom, strTo, msgRoot.as_string())
         return m1
 
-def Get_Num_Legs(reg,lr,epoch_no,batch_size,opt,activation,dropout):
+def Get_Num_Legs(reg,lr,epoch_no,batch_size,opt,activation,dropout, momentum, decay, mean_error_type):
     leg_num = -1
-    list_params = [] #parameters that have leg variants so are in list form
     if reg != None:
         if type(reg) is list:
             if type(reg[0]) is list:
@@ -239,10 +303,33 @@ def Get_Num_Legs(reg,lr,epoch_no,batch_size,opt,activation,dropout):
     if type(activation) is list:
         if len(activation) > leg_num:
             leg_num = len(activation)
-            
-    return leg_num, list_params
 
 
+    if type(dropout) is list:
+        if len(dropout) > leg_num:
+            leg_num = len(dropout)
+
+    if type(momentum) is list:
+        if len(momentum) > leg_num:
+            leg_num = len(momentum)
+
+    if type(decay) is list:
+        if len(decay) > leg_num:
+            leg_num = len(decay)
+    
+    if type(mean_error_type) is list:
+        if len(mean_error_type) > leg_num:
+            leg_num = len(mean_error_type)
+          
+    return leg_num
+
+def Get_Num_Legs_Trained(folder_name,load_partially_trained_model):
+    if load_partially_trained_model == True:
+        m1=NNmodel('evo',['mass', 'age', 'feh', 'Y', 'MLT'], ['radius','Teff', 'delnu'], Teff_scaling=5000)
+        m1.loadHist('drive/My Drive/4th Year Project/{}/Hist{}'.format(folder_name,folder_name),'pickle')
+        return len(m1.history['legs'])
+    else:
+        return 0
 
         
 
